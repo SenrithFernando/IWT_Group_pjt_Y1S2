3356a2c6e0eb8597135d6abeeb24a7508222c915
Senrith Fernando <rihansenrithvta@gmail.com>
2024-04-25 10:23:45 +0530

    create cards on administrator panel dashboard

--- administrator_panel/page/home.php
+++ administrator_panel/page/home.php
@@ -37,10 +37,41 @@ $useruid = $_SESSION['useruid'];
             <?php include "sidenav.php"; ?>
         </div>
         <div class="main-content">
+            <div class="dash_hed">
             <h2>Welcome, Admin!</h2><a href="../includes/logout.inc.php">Logout</a>
-            <p>This is the main content area of your admin panel.</p>
-            <iframe width="100%" height="500Vh" src="https://www.gallelaptop.lk/" frameborder="1"></iframe>
+            </div>
+            
+
+        <div class="dash_row">
+            <div class="dash_card">
+                <h2 class="dash_title">Card 1</h2>
+                <p class="dash_description">This is the description for Card 1.</p>
+                <img src="image1.jpg" alt="Image 1" class="dash_image">
+            </div>
+            <div class="dash_card">
+                <h2 class="dash_title">Card 2</h2>
+                <p class="dash_description">This is the description for Card 2.</p>
+                <img src="image2.jpg" alt="Image 2" class="dash_image">
+            </div>
+            <div class="dash_card">
+                <h2 class="dash_title">Card 3</h2>
+                <p class="dash_description">This is the description for Card 3.</p>
+                <img src="image3.jpg" alt="Image 3" class="dash_image">
+            </div>
+            <div class="dash_card">
+                <h2 class="dash_title">Card 4</h2>
+                <p class="dash_description">This is the description for Card 4.</p>
+                <img src="image4.jpg" alt="Image 4" class="dash_image">
+            </div>
         </div>
+
+
+
+
+
+            <!-- <p>This is the main content area of your admin panel.</p>
+            <iframe width="100%" height="500Vh" src="https://www.gallelaptop.lk/" frameborder="1"></iframe>
+         -->
     </div>
 </body>
 
