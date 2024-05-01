0c6e5abccfcca1433f35b31877702ffaa7108c38
Senrith Fernando <rihansenrithvta@gmail.com>
2024-05-01 20:49:55 +0530

    fille_path

---
+++ library/footer.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+  <div class="contact">
+    <h3>Contact Us</h3>
+    <p>Email: <a href="mailto:onlineexamination@gmail.com">onlineexamination@gmail.com</a></p>
+    <p>Phone No: <a href="tel:+94714563452">+94 71 456 3452</a></p>
+    <p>Address: No.452/2, Rajagiriya Rd, Colombo</p>
+  </div>
+  <div class="quick-links">
+    <p><strong>Quick Links</strong></p>
+    <a href="#">About</a>
+    <a href="#">Contact</a>
+    <a href="#">FAQs</a>
+  </div>
+  <div class="social-icons">
+    <p><strong>Follow Us</strong></p>
+    <a href="#"><i class="fab fa-facebook"></i></a>
+    <a href="#"><i class="fab fa-twitter"></i></a>
+    <a href="#"><i class="fab fa-instagram"></i></a>
+    <a href="#"><i class="fab fa-youtube"></i></a>
+  </div>
+
+  <p>&copy; 2024 All rights reserved.</p>
+</footer>
